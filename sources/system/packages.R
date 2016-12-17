70554f7387d7ff1640a43fc942bac47d71033fb3
samuelmontoyag <samuelmontoyag@gmail.com>
2016-12-17 14:01:54 -0300

    first commit

---
+++ sources/system/packages.R
@@ -0,0 +1,10 @@
+
+#====================================================
+#A)Configuration
+#====================================================
+#Packages
+cat("- Loading packages...\n")
+if (!require("phangorn")) install.packages("phangorn")
+library("phangorn")
+if (!require("ape")) install.packages("ape")
+library("ape")
