de359948b751a3909a3a334594bc3fa4d394c30a
samuelmontoyag <samuelmontoyag@gmail.com>
2017-01-04 18:57:10 -0300

    ADD: agrega nueva version de subarboles

--- python/subarboles.R
+++ python/subarboles.R
@@ -1,10 +1,36 @@
-#Obtiene subarboles
-# setwd("C:/Users/elper/Downloads/Proyecto 1") #Cambiar por tu directorio
-# source("C:/Users/elper/Downloads/Proyecto 1/sources/system/packages.R") 
-# install.packages("Rserve")
+# setwd("/Users/maurorios/Downloads/Proyecto_filogenia/Proyecto 1") #Cambiar por tu directorio
+# source("/Users/maurorios/Downloads/Proyecto_filogenia/Proyecto 1/sources/system/packages.R")         #Carga un conjunto de paquetes
+library(ape)
 library(phangorn)
-tree1=read.tree("uploads/tree1.tree")
-stree1=subtrees(tree1)                  #subárboles tree1
-print(length(stree1))
+cat("\014")                                 #Limpiar consola
+
+tree1=read.tree("data/tree1.tree")
+tree2=read.tree("data/tree1.tree")
+
+stree1=subtrees(tree1)                  #sub?rboles tree1
+stree2=subtrees(tree2)                  #sub?rboles tree2
+matriz=matrix(0,length(stree1),length(stree2))
 for (a in 1:length(stree1))
-  print(stree1[a])
+{ {for (b in 1:length(stree2))
+  matriz[a,b]=as.numeric(all.equal(stree1[[a]],stree2[[b]]))
+}
+}
+info = ''
+texto =''
+contador = 1
+for( c in 1:dim(matriz)[1]){
+  for( d in 1:dim(matriz)[2]){
+    if(matriz[c,d] == 1){
+        print(paste('arbol',contador))
+        print('informacion del arbol :')
+        print(paste('numero de nodos :',stree1[[c]]$Nnode))
+        print(paste('numero de hojas :',stree1[[c]]$Ntip))
+        contador = contador + 1
+      for(e in 1:length(stree1[[c]]$tip)){
+        info = stree1[[c]]$tip.label[e]
+        print(info)
+      }
+      print('---')
+    }
+  }
+}
